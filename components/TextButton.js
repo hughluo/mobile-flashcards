24c1772a6e712d5e2423cda0a1eb955f609854ff
wexort <bugwriter@outlook.com>
2021-03-08 22:17:44 +0100

    add TextButton

---
+++ components/TextButton.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Pressable, StyleSheet, Text } from 'react-native';
+
+function TextButton({ text, onPress }) {
+	return (
+		<Pressable style={styles.button} onPress={onPress}>
+			<Text style={styles.textStyle}>{text}</Text>
+		</Pressable>
+	);
+}
+
+const styles = StyleSheet.create({
+	button: {
+		borderRadius: 20,
+		padding: 10,
+		elevation: 2,
+		backgroundColor: 'tomato'
+	},
+	textStyle: {
+		color: 'white',
+		fontWeight: 'bold',
+		textAlign: 'center'
+	}
+});
+
+export default TextButton;
