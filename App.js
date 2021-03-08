e67cd675f761ea94bc7ee4646d21e72a823cbb01
wexort <bugwriter@outlook.com>
2021-03-08 19:46:14 +0100

    add Tab

--- App.js
+++ App.js
@@ -1,21 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+function DecksScreen() {
+	return (
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+			<Text>DecksScreen!</Text>
+		</View>
+	);
+}
+
+function NewDeckScreen() {
+	return (
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+			<Text>NewDeckScreen!</Text>
+		</View>
+	);
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<NavigationContainer>
+			<Tab.Navigator
+				screenOptions={({ route }) => ({
+					tabBarIcon: ({ focused, color, size }) => {
+						let iconName;
+
+						if (route.name === 'Decks') {
+							iconName = focused ? 'ios-albums' : 'ios-albums-outline';
+						} else if (route.name === 'Add Deck') {
+							iconName = focused ? 'ios-add-circle' : 'ios-add-circle-outline';
+						}
+
+						return <Ionicons name={iconName} size={size} color={color} />;
+					}
+				})}
+				tabBarOptions={{
+					activeTintColor: 'tomato',
+					inactiveTintColor: 'gray'
+				}}
+			>
+				<Tab.Screen name="Decks" component={DecksScreen} />
+				<Tab.Screen name="Add Deck" component={NewDeckScreen} />
+			</Tab.Navigator>
+		</NavigationContainer>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
 });
