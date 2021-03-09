57a0757d159c2182542c93c1e18ede74630eaf87
wexort <bugwriter@outlook.com>
2021-03-09 11:25:57 +0100

    extract Navigator from App, use nested Navigator Stack(Tab)

--- App.js
+++ App.js
@@ -1,16 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
 
-import DeckList from './components/DeckList';
-import AddDeck from './components/AddDeck';
+import { NavigationContainer } from '@react-navigation/native';
+import Navigator from './components/Navigator';
 
 function AppStatusBar() {
 	return (
@@ -20,52 +17,12 @@ function AppStatusBar() {
 	);
 }
 
-function DecksScreen() {
-	return (
-		<View style={{ flex: 1 }}>
-			<DeckList />
-		</View>
-	);
-}
-
-function NewDeckScreen() {
-	return (
-		<View style={{ flex: 1 }}>
-			<AddDeck />
-		</View>
-	);
-}
-
-const Tab = createBottomTabNavigator();
-
 export default function App() {
 	return (
 		<Provider store={createStore(reducer)}>
 			<NavigationContainer style={styles.container}>
 				<AppStatusBar />
-
-				<Tab.Navigator
-					screenOptions={({ route }) => ({
-						tabBarIcon: ({ focused, color, size }) => {
-							let iconName;
-
-							if (route.name === 'Decks') {
-								iconName = focused ? 'ios-albums' : 'ios-albums-outline';
-							} else if (route.name === 'Add Deck') {
-								iconName = focused ? 'ios-add-circle' : 'ios-add-circle-outline';
-							}
-
-							return <Ionicons name={iconName} size={size} color={color} />;
-						}
-					})}
-					tabBarOptions={{
-						activeTintColor: 'tomato',
-						inactiveTintColor: 'gray'
-					}}
-				>
-					<Tab.Screen name="Decks" component={DecksScreen} />
-					<Tab.Screen name="Add Deck" component={NewDeckScreen} />
-				</Tab.Navigator>
+				<Navigator />
 			</NavigationContainer>
 		</Provider>
 	);
---
+++ components/Navigator.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { View } from 'react-native';
+
+import DeckList from './DeckList';
+import AddDeck from './AddDeck';
+
+function DecksScreen() {
+	return (
+		<View style={{ flex: 1 }}>
+			<DeckList />
+		</View>
+	);
+}
+
+function NewDeckScreen() {
+	return (
+		<View style={{ flex: 1 }}>
+			<AddDeck />
+		</View>
+	);
+}
+
+const Tab = createBottomTabNavigator();
+function TabNavigator() {
+	return (
+		<Tab.Navigator
+			screenOptions={({ route }) => ({
+				tabBarIcon: ({ focused, color, size }) => {
+					let iconName;
+
+					if (route.name === 'Decks') {
+						iconName = focused ? 'ios-albums' : 'ios-albums-outline';
+					} else if (route.name === 'Add Deck') {
+						iconName = focused ? 'ios-add-circle' : 'ios-add-circle-outline';
+					}
+
+					return <Ionicons name={iconName} size={size} color={color} />;
+				}
+			})}
+			tabBarOptions={{
+				activeTintColor: 'tomato',
+				inactiveTintColor: 'gray'
+			}}
+		>
+			<Tab.Screen name="Decks" component={DecksScreen} />
+			<Tab.Screen name="Add Deck" component={NewDeckScreen} />
+		</Tab.Navigator>
+	);
+}
+
+const Stack = createStackNavigator();
+
+function Navigator() {
+	return (
+		<Stack.Navigator>
+			<Stack.Screen name="Home" component={TabNavigator} />
+		</Stack.Navigator>
+	);
+}
+
+export default Navigator;
