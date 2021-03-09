daf88a3044a81e421a1380c5ea6746fe07494690
wexort <bugwriter@outlook.com>
2021-03-09 16:37:25 +0100

    impl handleAddCard, handleStartQuiz

--- components/DeckDetail.js
+++ components/DeckDetail.js
@@ -3,25 +3,31 @@ import { StyleSheet, View, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { useNavigation } from '@react-navigation/native';
 
+import { startQuiz } from '../actions/quiz';
+
 import TextButton from './TextButton';
 
-function DeckDetail({ deck }) {
+function DeckDetail({ deck, dispatch }) {
 	const navigation = useNavigation();
-
+	const { title, questions } = deck;
+	const deckTitle = title;
+	const questionAmount = questions.length;
 	const handleAddCard = () => {
 		console.log('Add Card Pressed!');
-		navigation.navigate('Add Card', { title: deck.title });
+		navigation.navigate('Add Card', { deckTitle });
 	};
 	const handleStartQuiz = () => {
 		console.log('Start Quiz Pressed!');
+		dispatch(startQuiz(deckTitle, questionAmount));
+		navigation.navigate('Quiz', { deckTitle });
 	};
 	return (
 		<View style={styles.centeredView}>
-			<Text style={{ fontSize: 50 }}>{deck.title}</Text>
-			<Text style={{ fontSize: 16, color: 'gray' }}>{deck.questions.length} Questions</Text>
+			<Text style={{ fontSize: 50 }}>{`Deck '${deckTitle}'`}</Text>
+			<Text style={{ fontSize: 32, color: 'gray' }}>{questionAmount} Questions</Text>
 			<View style={styles.buttonGroup}>
-				<TextButton text="Add Card" onPress={handleAddCard} />
-				<TextButton text="Start Quiz" onPress={handleStartQuiz} />
+				<TextButton buttonColor="black" text="Add Card" onPress={() => handleAddCard()} />
+				<TextButton text="Start Quiz" onPress={() => handleStartQuiz()} />
 			</View>
 		</View>
 	);
@@ -39,12 +45,13 @@ const styles = StyleSheet.create({
 		marginTop: 22
 	},
 	buttonGroup: {
+		alignSelf: 'stretch',
 		padding: 20,
 		margin: 10
 	}
 });
 
-function mapStateToProps(decks, { route }) {
+function mapStateToProps({ decks }, { route }) {
 	const { title } = route.params;
 
 	return {
